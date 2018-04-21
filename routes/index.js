ee376c6f8b37476c8e1e4efbb4af51639a8794f6
Dakota Crouchelli <dhc710@yahoo.com>
2018-04-21 16:11:59 -0400

    more changes to routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const inventoryDB = data.inventory
 // Configures the app
 module.exports = function (app)
 {
-	/* Cookie flagging middleware
+	// Cookie flagging middleware
 	// Searches for a cookie in every request, before it reaches the real routes
 	// If a cookie is found vith a valid session-id, the request is marked with that user's index
 	app.use(async function (req, res, next)
@@ -13,14 +13,22 @@ module.exports = function (app)
 		delete req.authUser
 
 		if (req.cookies.AuthCookie)
-			for (var i=0; i<users.length; i++)
-				for (var j=0; j<users[i].sessions.length; j++)
-					if (users[i].sessions[j] === req.cookies.AuthCookie)
-						req.authUser = i
+		{
+			try
+			{
+				const user = await usersDB.getUserBySession(req.cookies.AuthCookie)
+
+				req.authUser = user._id
+			}
+
+			catch (err)
+			{
+				console.log(err)
+			}
+		}
 
 		next()
 	})
-	*/
 
 	app.get("/", async function (req, res)
 	{
@@ -34,17 +42,56 @@ module.exports = function (app)
 
 	app.post("/login", async function (req, res)
 	{
-		res.render("login")
-	})
+		try
+		{
+			if (!req.body.username || (typeof req.body.username) !== "string")
+				throw "No username in request body"
+			
+			
+			if (!req.body.password || (typeof req.body.password) !== "string")
+				throw "No password in request body"
+			
+			user = await usersDB.getUserByEmail(req.body.username)
 
-	app.get("/product", async function (req, res)
-	{
-		res.render("product")
+			if (!(await bcrypt.compare(req.body.password, user.hashedPassword)))
+				throw "Password incorrect"
+			
+			const expiresAt = new Date();
+			expiresAt.setHours(expiresAt.getHours() + 1);
+			const sessionID = uuid();
+			res.cookie("AuthCookie", sessionID, { expires: expiresAt });
+			await setUserSession(user._id, sessionID)
+			
+			res.redirect("/home")
+		}
+	
+		catch (err)
+		{
+			/*res.status(400).render("login",
+			{
+				"error-occurred": true,
+				"error-text": err
+			})
+			*/
+			console.log(err)
+		}
+		
+		res.render("home")
 	})
 
 	app.get("/register", async function (req, res)
 	{
 		res.render("register")
+	})
+
+	app.post("/register", async function (req, res)
+	{
+		res.render("register")
+	})
+
+	app.get("/product", async function (req, res)
+	{
+		res.render("product")
 	})
 
 	app.get("/cart", async function (req, res)
