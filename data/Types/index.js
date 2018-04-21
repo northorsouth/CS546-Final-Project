3f90d6bf63a5fcf8abaf3876eb7852e8f80a1b7e
villa7 <villa7.kluge5@gmail.com>
2018-04-21 14:42:21 -0400

    add users, purchases, cart

---
+++ data/Types/index.js
@@ -0,0 +1,30 @@
+const itemType = {
+	_id: "string",
+	name: "string",
+	price: Number,
+};
+const sellerType = {
+	_id: "string",
+	name: "string",
+	shopowner: Boolean,
+};
+const commentType = {
+	_id: 'string',
+	poster: sellerType,
+	comment: 'string',
+	rating: Number,
+	timestamp: Date,
+};
+const purchaseType = {
+	_id: 'string',
+	item: itemType,
+	purchasePrice: Number,
+	timestamp: Date,
+};
+
+module.exports = {
+	itemType,
+	sellerType,
+	commentType,
+	purchaseType,
+};
