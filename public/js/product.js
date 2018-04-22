76d2bcaba73152776c958f96411e0433e8f550e7
villa7 <villa7.kluge5@gmail.com>
2018-04-22 14:06:56 -0400

    stars

--- public/js/product.js
+++ public/js/product.js
@@ -2,6 +2,7 @@
 (function() {
 	const pElem = $('#cart-list');
 	const starTemplate = template($('#star-template').innerHTML);
+	const starInputTemplate = template($('#star-input-template').innerHTML);
 
 	function fillItemDetails(res, xhr) {
 		const item = JSON.parse(res);
@@ -9,6 +10,16 @@
 			return a + c.rating;
 		}, 0)) / item.comments.length)).fill(starTemplate()).join('') : 'No ratings';
 		$('#rating').innerHTML = averageRating;
+	}
+
+	function addInputStars() {
+		for (let i = 1; i <= 5; i++) {
+			if (i < 5) $('#star-rating').insertAdjacentHTML('beforeend', starInputTemplate({val: i}));
+			else $('#star-rating').insertAdjacentHTML('beforeend', starInputTemplate({val: i, checked: 'checked'}));
+		}
+		$('#star-rating').addEventListener('click', function(e) {
+			console.log(e.target)
+		});
 	}
 
 	const pid = location.href.split('/').reverse()[0];
@@ -21,5 +32,7 @@
 			$('.error').innerText = res;
 		}
 	});
+
+	addInputStars();
 
 })();
