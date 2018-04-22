4b8b8e404fe332d8fed5734b40f4acb5458caade
villa7 <villa7.kluge5@gmail.com>
2018-04-22 12:23:12 -0400

    css folder

---
+++ public/js/cart.js
@@ -0,0 +1,35 @@
+'use strict';
+(function() {
+	const plistElem = $('#cart-list');
+	const productTemplate = template($('#cart-template').innerHTML);
+
+	function fillProductList(items, xhr) {
+		items = JSON.parse(items);
+		plistElem.innerHTML = '';
+		for (const item of items) {
+
+			if (!item.count) continue;
+
+			const averageRating = item.comments.length ? Array(Math.round((item.comments.reduce((a, c) => {
+				return a + c.rating;
+			}, 0)) / item.comments.length)).fill(starTemplate()).join('') : 'No ratings';
+
+			const displayItem = {
+				_id: item._id,
+				name: item.item.name, 
+				price: '$' + item.item.price,
+				image: '/api/public/image/' + item._id
+			};
+			plistElem.insertAdjacentHTML('beforeend', productTemplate(displayItem));
+		}
+	}
+
+	Ajax.get({
+		url: '/api/public/inventory',
+		success: fillProductList,
+		error: function(res, xhr) {
+			plist_elem.innerText = res;
+		}
+	});
+
+})();
